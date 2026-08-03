c821c4da0cedec051142166f0b8c32e959634d23
Laye100 <layediouf7991@gmail.com>
2026-08-03 21:08:56 +0000

    feat: affichage des statistiques du catalogue via compréhensions (Partie 6)

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ while active:
     print("4. trier les datasets")
     print("5. Supprimer un dataset")
     print("6. Modifier un dataset")
-    print("7. Quitter \n")
+    print("7. Voir les statistiques des datasets")
+    print("8. Quitter \n")
 
     choix = input("Entrez votre choix (1-7): ")
 
@@ -128,8 +129,41 @@ while active:
         else:
             print("Dataset non trouvé.")
 
-
     elif choix == "7":
+        # les statistiques des datasets
+        if not catalogue_data:
+            print("Aucun dataset enregistré pour le moment.")
+        else:
+            total_datasets = len(catalogue_data)
+            total_lignes = sum(dataset["nbr_lignes"] for dataset in catalogue_data)
+            moyenne_colonnes = sum(dataset["nbr_colonnes"] for dataset in catalogue_data) / total_datasets if total_datasets > 0 else 0
+            total_taille = sum(dataset["taille"] for dataset in catalogue_data)
+            data_public = sum(1 for dataset in catalogue_data if dataset["public"].lower() == "true")
+            data_prive = total_datasets - data_public
+            nb_csv = sum(1 for dataset in catalogue_data if dataset["format"].lower() == "csv")
+            nb_json = sum(1 for dataset in catalogue_data if dataset["format"].lower() == "json")
+            rp_domaine = {}
+            for dataset in catalogue_data:
+                domaine = dataset["domaine"]
+                if domaine in rp_domaine:
+                    rp_domaine[domaine] += 1
+                else:
+                    rp_domaine[domaine] = 1
+
+            print("\n--- Statistiques des datasets ---")
+            print(f"Nombre total de datasets : {total_datasets}")
+            print(f"Nombre total de lignes   : {total_lignes}")
+            print(f"Moyenne de colonnes      : {moyenne_colonnes:.2f}")
+            print(f"Taille totale des datasets : {total_taille} Mo")
+            print(f"Datasets publics : {data_public}")
+            print(f"Datasets privés : {data_prive}")
+            print(f"Nombre de datasets au format CSV : {nb_csv}")
+            print(f"Nombre de datasets au format JSON : {nb_json}")
+            print("Répartition des datasets par domaine :")
+            for domaine, count in rp_domaine.items():
+                print(f"- {domaine} : {count} datasets")
+
+    elif choix == "8":
         # Quitter le programme
         print("Au revoir !")
         active = False
